d3f10b223950bb8c992a6dc24eab4654496f75a3
David Zeit <dzeit216@gmail.com>
2018-05-04 00:06:07 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,8 +2,10 @@ require 'pry'
 
 class Hash
   def keys_of(*arguments)
+    animal_list = []
     arguments.each do |animal, location|
-      arguments.include?(animal)
+      if arguments.include?(animal) 
+        animal_list << animal 
       binding.pry 
     end 
     return []
