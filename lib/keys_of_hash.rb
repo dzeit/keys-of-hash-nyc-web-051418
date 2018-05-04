486e7966e479b420e890e8079f781e57e0fe0c5b
David Zeit <dzeit216@gmail.com>
2018-05-04 00:09:09 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,6 +4,7 @@ class Hash
   def keys_of(*arguments)
     animal_list = []
     arguments.each do |animal, location|
+      binding.pry 
       if arguments.include?(animal) 
         animal_list << animal 
       end 
