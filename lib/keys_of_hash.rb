652a2a65269933d611e68089427332a8e5cf6cb1
David Zeit <dzeit216@gmail.com>
2018-05-04 00:35:22 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,11 +1,11 @@
 require 'pry'
 
 class Hash
-  def keys_of(*arguments)
+  def keys_of(*arguments) # * means splat which means that can (but not necessarily) include several arguments
     animal_list = []
     arguments.each do |location|
-      self.each do |animal, country|
-        if country == location 
+      self.each do |animal, country| # now we iterate through the called on instance (self) 
+        if country == location # check instances values for one of our locations, if it exists we push the key to later return (line 13)
           animal_list << animal  
         end 
       end 
